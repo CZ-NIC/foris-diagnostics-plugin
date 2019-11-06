a20e4af814ff48493793df9a62a462a33db41b29
Stepan Henek <stepan.henek@nic.cz>
2019-11-06 14:33:37 +0100

    version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class BuildCmd(build_py):
 
 setup(
     name="Foris Diagnostics Plugin",
-    version="12",
+    version="12.1",
     description="Diagnostics plugin for foris web interfce",
     author="CZ.NIC, z. s. p. o.",
     author_email="stepan.henek@nic.cz",
